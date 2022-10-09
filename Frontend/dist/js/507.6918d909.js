c43a172c7fc3e1726894758dba4bd24ab981d6a9
MarianaYaz <62482852+MarianaYaz@users.noreply.github.com>
2022-10-09 14:31:20 -0500

    Commit files
    Co-Authored-By: Gustavo825 <41050062+Gustavo825@users.noreply.github.com>

---
+++ Frontend/dist/js/507.6918d909.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkfeiorder_fronted"]=self["webpackChunkfeiorder_fronted"]||[]).push([[507],{507:function(e,n,t){t.r(n),t.d(n,{default:function(){return a}});var r=function(){var e=this;e._self._c;return e._m(0)},u=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=t(1),f={},i=(0,s.Z)(f,r,u,!1,null,null,null),a=i.exports}}]);
+//# sourceMappingURL=507.6918d909.js.map
